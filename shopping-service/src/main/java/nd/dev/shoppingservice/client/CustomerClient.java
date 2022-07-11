adcd1de17031fec1aaf627cf63aaa5addc9b22ba
Nicolas Dankiewicz <nicodanke@Nicolass-MBP-2.fibertel.com.ar>
2022-07-10 21:09:08 -0300

    a

--- shopping-service/src/main/java/nd/dev/shoppingservice/client/CustomerClient.java
+++ shopping-service/src/main/java/nd/dev/shoppingservice/client/CustomerClient.java
@@ -1,14 +1,25 @@
 package nd.dev.shoppingservice.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import nd.dev.shoppingservice.model.Customer;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "customer-service", fallback = CustomerHystrixFallbackFactory.class)
+@FeignClient(name = "customer-service")
 public interface CustomerClient {
 
     @GetMapping(value = "/customers/{id}")
+    @CircuitBreaker(name = "customersCB", fallbackMethod = "getCustomerFallback")
     public ResponseEntity<Customer> getCustomer(@PathVariable("id") long id);
+
+    default ResponseEntity<Customer> getCustomerFallback(long id) {
+        Customer customer = Customer.builder()
+                .firstName("none")
+                .lastName("none")
+                .email("none")
+                .photoUrl("none").build();
+        return ResponseEntity.ok(customer);
+    }
 }
